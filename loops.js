e54a01c7befc53ebb1ac6e59eb82d4927f4bd1be
Haley Someone <hbarlar1@gmail.com>
2018-07-13 18:07:47 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -19,10 +19,10 @@ function whileLoop(number) {
 }
 
   function doWhileLoop(array) {
-    
-    function maybeTrue() {
-    return Math.random() >= 0.5
+      function maybeTrue() {
+      return Math.random() >= 0.5
+      }
+    while () {
+      
     }
-    
-    
-  }
+}
